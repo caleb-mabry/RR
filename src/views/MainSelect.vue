0c28b3d7f1aca238d7a4d9fb9191c4199a4ad397
caleb.mabry.15 <caleb.mabry.15@cnu.edu>
2020-08-11 14:45:31 -0400

    Fixed a bug with .jpg and .png not dsplaying

--- src/views/MainSelect.vue
+++ src/views/MainSelect.vue
@@ -1,57 +1,75 @@
 <template>
-<div>
+  <div>
     <div v-if="doesCharacterEpisodeExist" class="container">
-        <router-link to="/Characters">Back</router-link>
-            <img v-for="item in charactersInEpisode" :key="item.id" :src="require('../assets/'+item+'.jpg')" class="tile-image" />
+      <router-link to="/Characters">Back</router-link>
+      <img v-for="item in charactersInEpisode" :key="item.id" :src="imageName(item)" class="tile-image" />
     </div>
-  <div v-else  class="container">
-     <router-link v-for="item in allCharacters" :key="item.id" :to="{ name:'Episode', params: {characterEpisode: item}}" class="link"> <img :src="require('../assets/'+item+'.png')" class="tile-image" /></router-link >
+    <div v-else class="container">
+      <router-link
+        v-for="item in allCharacters"
+        :key="item.id"
+        :to="{ name:'Episode', params: {characterEpisode: item}}"
+        class="link"
+      >
+        <img :src="require('../assets/'+item+'.png')" class="tile-image" />
+      </router-link>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
-import Characters from '../assets/Characters.json'
+import Characters from "../assets/Characters.json";
+
 export default {
-    data() {
-        return {
-            selectedEpisode: this.$route.params.characterEpisode
-        }
+  data() {
+    return {
+      selectedEpisode: this.$route.params.characterEpisode,
+      selectedCharacterPath: "",
+    };
+  },
+  methods: {
+    imageName(name) {
+      try {
+        return require("../assets/" + name + ".png");
+      } catch {
+        return require("../assets/" + name + ".jpg");
+      }
     },
-computed: {
-    allCharacters() {        
-        return Object.keys(Characters)
+  },
+  computed: {
+    allCharacters() {
+      return Object.keys(Characters);
     },
     doesCharacterEpisodeExist() {
-        if (this.$route.params.characterEpisode == undefined) {
-            return false;
-        }
-        else {
-            return true;
-        }
+      if (this.$route.params.characterEpisode == undefined) {
+        return false;
+      } else {
+        return true;
+      }
     },
     charactersInEpisode() {
-        if (this.$route.params.characterEpisode) {
-            return Object.keys(Characters[this.$route.params.characterEpisode]) 
-        }
-        else {
-            return []
-        }
-    }
-},
-}
+      if (this.$route.params.characterEpisode) {
+        console.log(
+          Object.keys(Characters[this.$route.params.characterEpisode])
+        );
+        return Object.keys(Characters[this.$route.params.characterEpisode]);
+      } else {
+        return [];
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
 .container {
-    display: flex;
-    width: 50%;
+  display: flex;
+  width: 50%;
 }
 .link {
-    padding: 0px;
-    
+  padding: 0px;
 }
 .tile-image {
-    width: 100%;
+  width: 100%;
 }
 </style>
