85c0c98284ae5e01af88711f98d495d8b358f54d
Caleb Mabry <caleb.mabry.15@cnu.edu>
2020-08-12 15:40:36 -0400

    Added Google Analytics!

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 
+Vue.use(VueAnalytics, {
+  id: "UA-158287109-4",
+  router
+})
 Vue.config.productionTip = false
 
 new Vue({
