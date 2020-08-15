15e209ad0f10a913e03943905baba121a3c892ce
caleb.mabry.15 <caleb.mabry.15@cnu.edu>
2020-08-15 14:33:09 -0400

    Changed a bunch of crap with the wwebpack configuration. Dont know what I did so undid it

--- vue.config.js
+++ vue.config.js
@@ -10,24 +10,31 @@ module.exports = {
       skipWaiting: true,
       clientsClaim: true,
     },
-    chainWebpack(config) {
-      config.plugins.delete('prefetch');
+    // chainWebpack(config) {
+    //   config.plugins.delete('prefetch');
       
-      // and this line 
-      config.plugin('CompressionPlugin').use(CompressionPlugin);
-    },
-    chainWebpack: config => {
-      // remove the prefetch plugin
-      config.plugins.delete('prefetch')
+    //   // and this line 
+    //   config.plugin('CompressionPlugin').use(CompressionPlugin);
+    // },
+    // chainWebpack: config => {
+
+    //   config.module
+    //   .rule('images')
+    //     .use('url-loader')
+    //       .loader('url-loader')
+    //       .tap(options => Object.assign(options, { limit: 10240 }))
+
+    //   // remove the prefetch plugin
+    //   config.plugins.delete('prefetch')
   
-      // or:
-      // modify its options:
-      config.plugin('prefetch').tap(options => {
-        options[0].fileBlacklist = options[0].fileBlacklist || []
-        options[0].fileBlacklist.push(/myasyncRoute(.)+?\.js$/)
-        return options
-      })
-    }
+    //   // or:
+    //   // modify its options:
+    //   config.plugin('prefetch').tap(options => {
+    //     options[0].fileBlacklist = options[0].fileBlacklist || []
+    //     options[0].fileBlacklist.push(/myasyncRoute(.)+?\.js$/)
+    //     return options
+    //   })
+    // }
   
   
   }
