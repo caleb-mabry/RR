183dbf3d3f92aa28169141c2e50051939b7158b6
caleb.mabry.15 <caleb.mabry.15@cnu.edu>
2020-08-15 18:19:01 -0400

    Working on preloading all information to increase page loadtime

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const CompressionPlugin = require('compression-webpack-plugin');
 const PreloadWebpackPlugin = require('preload-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   pwa: {
     name: 'ripping-resource',
@@ -12,32 +13,9 @@ module.exports = {
       clientsClaim: true,
     },
 
-    chainWebpack: config => {
-      config.plugins.delete('prefetch');
-      
-      // aEnable Text Compression
-      config.plugin('CompressionPlugin').use(CompressionPlugin);
-
-      config.module
-      .rule('images')
-        .use('url-loader')
-          .loader('url-loader')
-          .tap(options => Object.assign(options, { limit: 10240 }))
-
-      // remove the prefetch plugin
-      config.plugins.delete('prefetch')
-  
-      // or:
-      // modify its options:
-      config.plugin('prefetch').tap(options => {
-        options[0].fileBlacklist = options[0].fileBlacklist || []
-        options[0].fileBlacklist.push(/myasyncRoute(.)+?\.js$/)
-        options[0].fileBlacklist.push(/myasyncRoute(.)+?\.css$/)
-        return options
-      })
-    },
     configureWebpack: {
       plugins: [
+        new HtmlWebpackPlugin(),
         new PreloadWebpackPlugin()
       ]
     }
