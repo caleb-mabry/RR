ced316afd4ece22416e45289321b11aebec47774
caleb.mabry.15 <caleb.mabry.15@cnu.edu>
2020-08-15 14:02:13 -0400

    Lazy loading the home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import MainSelect from '../views/MainSelect.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: () => import('../views/Home.vue')
   },
   {
     path: '/Characters',
